2d2ffd993850cfd1afeea470273e1dce8ae37cb2
gnsvno <selvapp18@protonmail.ch>
2018-05-29 21:39:37 +0800

    Fixed all validation bugs, some additions in docker configs

--- BitcoindTxValidator.js
+++ BitcoindTxValidator.js
@@ -2,51 +2,56 @@
 * Copyright (c) 2018, Gerald Selvino 
 * <gerald.selvino@protonmail.com> All rights reserved.
 *
-* The main node.js executable file.
+* The main node.js executable file. Listens at port
+* 10101 for call to service filterValidBitcoinTx.
+* It then parse the JSON object from the POST request
+* and check for valid bitcoin transactions. Pushes
+* those valid transactions to the database
 *******************************************************/
 var db = require ("./db.js");
-var txObj1 = require ("./transactions-1.json");
-var txObj2 = require ("./transactions-2.json");
-
-var allTx = [txObj1, txObj2];
 
 //Query the database to see if a record exists. Calls a callback
 //depending on the result of the query
-var queryExisting = function(querystring, cbparams, cbexists, cbnotexists) {
-    var query = db.client.query(querystring);
-    var isExists = false;
-    query
-    .on('error', 
-    function(err) {
-        console.error(err);
-        db.client.end();
-    })
-    .on('result',
-    function(row) {
-        isExists = true;
-    })
-    .on('end',
-    function() {
-        if(isExists) {
-            if (cbexists != null)
-                cbexists(cbparams);
-        } else {
-            if (cbnotexists != null)
-                cbnotexists(cbparams);
-        }
-    })
+var queryExisting = function(querystring, txparams) {
+    setTimeout(function() {
+        var query = db.client.query(querystring);
+        query
+        .on('error', 
+        function(err) {
+            console.error(err);
+            db.client.end();
+        })
+        .on('result',
+        function(row) {
+            txparams.isExists = true;
+        })
+        .on('end',
+        function() {
+            if(txparams.isExists === null) {
+                txparams.isExists = false;
+            }
+        })
+    }, 110 * txparams.txCounter);
 }
 
 //Insert valid transactions to database
-var insertTx = function(objparams) {
-    db.client.query("INSERT INTO transactions(address,amount,txid,vout) VALUES(?,?,?,?)", 
-    [objparams.address, objparams.amount, objparams.txid, objparams.vout], 
-    function(err, result) {
-        if (err) {
-            db.client.end();
-            console.error(err);
+var insertTx = function(objparams, txparams) {
+    var intervalHandle = setInterval(function() {
+        if (txparams.isExists !== undefined
+            && txparams.isExists !== null) {
+            if (txparams.isExists === false) {
+                db.client.query("INSERT INTO transactions(address,amount,txid,vout) VALUES(?,?,?,?)", 
+                [objparams.address, objparams.amount, objparams.txid, objparams.vout], 
+                function(err, result) {
+                    if (err) {
+                        db.client.end();
+                        console.error(err);
+                    }
+                });
+            }
+            clearInterval(intervalHandle);
         }
-    });
+    }, 100);
 }
 
 //Method to check if the number of digits in the amount is valid,
@@ -62,60 +67,55 @@ var isValidAmount =  function(amount) {
     return isvalid;
 }
 
-//The main function executed by the app timer below
-//filters the valid transactions and call function
-//to insert them in the database. 
+//The main function executed by the service below
+//filters the valid transactions and insert them
+//to the database. 
 var main = function(txtype, txobj) {
-    for (var tx in txobj) {
-        for (var txelem in txobj[tx].transactions) {
-            if (txobj[tx].transactions[txelem].confirmations >= 6 //There must be at least 6 confirmations
-                && txobj[tx].transactions[txelem].amount > 0 //Deposit amount must be greater than 0
-                && isValidAmount(txobj[tx].transactions[txelem].amount)) { //Amount must have a maximum of 8 decimals, because it is the specs of Bitcoin
+    var txCounter = 0;
+    var txExists = [];
+    for (var txelem in txobj.transactions) {
+        if (txobj.transactions[txelem].confirmations >= 6 //There must be at least 6 confirmations
+            && txobj.transactions[txelem].amount > 0 //Deposit amount must be greater than 0
+            && isValidAmount(txobj.transactions[txelem].amount)) { //Amount must have a maximum of 8 decimals, because it is the specs of Bitcoin
+            
+            var isTxTypeValid = false;
+            switch (txtype) {
+                case "deposit":
+                    if (txobj.transactions[txelem].category == "receive" //Transaction is a deposit
+                        || txobj.transactions[txelem].category == "generate") { //Transaction is mined
+                        isTxTypeValid = true;
+                    }
+                    break;
+                case "withdraw":
+                    if (txobj.transactions[txelem].category == "send") { //Transaction is a withdrawal
+                        isTxTypeValid = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+            if (isTxTypeValid) {
+                var address = txobj.transactions[txelem].address;
+                var amount = Number(txobj.transactions[txelem].amount.toFixed(8));
+                var txid = txobj.transactions[txelem].txid;
+                var vout = txobj.transactions[txelem].vout;
+                txExists.push({isExists: null, txCounter: txCounter});
+                //Check if there are duplicate entries already inserted in the database. In Bitcoin uniqueness is
+                //the combination of txid and vout, both must be unique to be able to say that it is a different transaction
+                queryExisting("SELECT txid FROM transactions WHERE txid='" + txid + "' OR vout=" + vout,
+                               txExists[txCounter]);
                 
-                var isTxTypeValid = false;
-                switch (txtype) {
-                    case "deposit":
-                        if (txobj[tx].transactions[txelem].category == "receive" //Transaction is a deposit
-                            || txobj[tx].transactions[txelem].category == "generate") { //Transaction is mined
-                            isTxTypeValid = true;
-                        }
-                        break;
-                    case "withdraw":
-                        if (txobj[tx].transactions[txelem].category == "send") { //Transaction is a withdrawal
-                            isTxTypeValid = true;
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                
-                if (isTxTypeValid) {
-                    var address = txobj[tx].transactions[txelem].address;
-                    var amount = Number(txobj[tx].transactions[txelem].amount.toFixed(8));
-                    var txid = txobj[tx].transactions[txelem].txid;
-                    var vout = txobj[tx].transactions[txelem].vout;
-                    
-                    //Check if there are duplicate entries already inserted in the database. In Bitcoin uniqueness is
-                    //the combination of txid and vout, both must be unique to be able to say that it is a different transaction
-                    queryExisting( "SELECT txid FROM transactions WHERE txid='" + txid + "' OR vout='" + vout + "'",
-                                { address: address, amount: amount, txid: txid, vout: vout }, //Values to be inserted in db
-                                null, //Callback if query exists
-                                insertTx ); //Insert the values in db if query does not exists 
-                }
+                //Async routine to check the isExists flag of each transaction and perform an insert
+                //in the database if the flag is false. It is being set in the function queryExisting
+                insertTx({address: address, amount: amount, txid: txid, vout: vout}, txExists[txCounter]); //Insert the values in db if query does not exists
+
+                ++txCounter;
             }
         }
     }
 }
 
-var timerhandle = setInterval(function() {
-    if (db.status == "connected") {
-        //main(message.txtype, message.data);
-        main("deposit", allTx);
-        console.log ("main executed");
-        clearInterval(timerhandle);
-    }
-}, 500);
-        
 require('seneca')() //Uses Seneca framework to start our microservice
 .add(
     {cmd: "filterValidBitcoinTx"},
@@ -124,8 +124,7 @@ require('seneca')() //Uses Seneca framework to start our microservice
         //we are connected to the database
         var timerhandle = setInterval(function() {
             if (db.status == "connected") {
-                //main(message.txtype, message.data);
-                main("deposit", allTx);
+                main(message.txtype, message.data);
                 console.log ("main executed");
                 clearInterval(timerhandle);
             }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # GNSBitcoind_Tx_Validator
-A simple microservice to filter and check for valid transactions from Bitcoind (https://en.bitcoin.it/wiki/Bitcoind) then stores those valid Tx on a database. It can be run natively or dockerized (the docker scripts are provided)
+A Node.js microservice to filter and check for valid transactions from Bitcoind (https://en.bitcoin.it/wiki/Bitcoind) then stores those valid Tx on a database. It can be run natively or dockerized (the docker scripts are provided)
 
 # Requirement
 To run it in a container, you need docker and docker-compose. All dependencies will be handled by docker then.
@@ -7,16 +7,16 @@ To run it in a container, you need docker and docker-compose. All dependencies w
 To run it in stand-alone, you need node.js, npm, mysql for node, seneca framework
 
 # Installation
-- Clone this repository i.e. git clone `https://github.com/geraldselvino/GNSBitcoind_Tx_Validator` 
+- Clone this repository i.e. `git clone https://github.com/geraldselvino/GNSBitcoind_Tx_Validator` 
 - Go to command line and change to cloned directory 
 
 **Docker** 
-- Type docker-compose up to start the microservice 
+- Type `docker-compose` up to start the microservice 
 
 **Stand-alone** 
 - Changed the host property in dbconfig.js to your hostname 
-- Type npm install 
-- Type npm start to start the microservice 
+- Type `npm install` 
+- Type `npm start` to start the microservice 
 
 # Usage
 - Call it like you would call any RESt APIs `http://<host>:10101/act?cmd=filterValidBitcoinTx&txtype=<deposit | withdraw>` and POST in the Http body the JSON object lists of transactions like the output of Bitcoind. 
